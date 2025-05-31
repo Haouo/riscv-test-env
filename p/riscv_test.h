cba36a8c7bce4582582bcf36b90bc9ba245d3f67
Haouo <q18131500@gs.ncku.edu.tw>
2025-05-31 15:46:15 +0800

    fix: riscv_test.h

--- p/riscv_test.h
+++ p/riscv_test.h
@@ -3,7 +3,7 @@
 #ifndef _ENV_PHYSICAL_SINGLE_CORE_H
 #define _ENV_PHYSICAL_SINGLE_CORE_H
 
-#include "../encoding.h"
+#include "encoding.h"
 
 //-----------------------------------------------------------------------
 // Begin Macro
@@ -23,11 +23,6 @@
   RVTEST_VECTOR_ENABLE;                                                 \
   .endm
 
-#define RVTEST_RV64UVX                                                  \
-  .macro init;                                                          \
-  RVTEST_ZVE32X_ENABLE;                                                 \
-  .endm
-
 #define RVTEST_RV32U                                                    \
   .macro init;                                                          \
   .endm
@@ -40,11 +35,6 @@
 #define RVTEST_RV32UV                                                   \
   .macro init;                                                          \
   RVTEST_VECTOR_ENABLE;                                                 \
-  .endm
-
-#define RVTEST_RV32UVX                                                  \
-  .macro init;                                                          \
-  RVTEST_ZVE32X_ENABLE;                                                 \
   .endm
 
 #define RVTEST_RV64M                                                    \
@@ -162,11 +152,6 @@
   csrwi fcsr, 0;                                                        \
   csrwi vcsr, 0;
 
-#define RVTEST_ZVE32X_ENABLE                                            \
-  li a0, (MSTATUS_VS & (MSTATUS_VS >> 1));                              \
-  csrs mstatus, a0;                                                     \
-  csrwi vcsr, 0;
-
 #define RISCV_MULTICORE_DISABLE                                         \
   csrr a0, mhartid;                                                     \
   1: bnez a0, 1b
@@ -180,73 +165,18 @@
 
 #define INTERRUPT_HANDLER j other_exception /* No interrupts should occur */
 
-#define RVTEST_CODE_BEGIN                                               \
-        .section .text.init;                                            \
-        .align  6;                                                      \
-        .weak stvec_handler;                                            \
-        .weak mtvec_handler;                                            \
-        .globl _start;                                                  \
-_start:                                                                 \
-        /* reset vector */                                              \
-        j reset_vector;                                                 \
-        .align 2;                                                       \
-trap_vector:                                                            \
-        /* test whether the test came from pass/fail */                 \
-        csrr t5, mcause;                                                \
-        li t6, CAUSE_USER_ECALL;                                        \
-        beq t5, t6, write_tohost;                                       \
-        li t6, CAUSE_SUPERVISOR_ECALL;                                  \
-        beq t5, t6, write_tohost;                                       \
-        li t6, CAUSE_MACHINE_ECALL;                                     \
-        beq t5, t6, write_tohost;                                       \
-        /* if an mtvec_handler is defined, jump to it */                \
-        la t5, mtvec_handler;                                           \
-        beqz t5, 1f;                                                    \
-        jr t5;                                                          \
-        /* was it an interrupt or an exception? */                      \
-  1:    csrr t5, mcause;                                                \
-        bgez t5, handle_exception;                                      \
-        INTERRUPT_HANDLER;                                              \
-handle_exception:                                                       \
-        /* we don't know how to handle whatever the exception was */    \
-  other_exception:                                                      \
-        /* some unhandlable exception occurred */                       \
-  1:    ori TESTNUM, TESTNUM, 1337;                                     \
-  write_tohost:                                                         \
-        sw TESTNUM, tohost, t5;                                         \
-        sw zero, tohost + 4, t5;                                        \
-        j write_tohost;                                                 \
-reset_vector:                                                           \
-        INIT_XREG;                                                      \
-        RISCV_MULTICORE_DISABLE;                                        \
-        INIT_RNMI;                                                      \
-        INIT_SATP;                                                      \
-        INIT_PMP;                                                       \
-        DELEGATE_NO_TRAPS;                                              \
-        li TESTNUM, 0;                                                  \
-        la t0, trap_vector;                                             \
-        csrw mtvec, t0;                                                 \
-        CHECK_XLEN;                                                     \
-        /* if an stvec_handler is defined, delegate exceptions to it */ \
-        la t0, stvec_handler;                                           \
-        beqz t0, 1f;                                                    \
-        csrw stvec, t0;                                                 \
-        li t0, (1 << CAUSE_LOAD_PAGE_FAULT) |                           \
-               (1 << CAUSE_STORE_PAGE_FAULT) |                          \
-               (1 << CAUSE_FETCH_PAGE_FAULT) |                          \
-               (1 << CAUSE_MISALIGNED_FETCH) |                          \
-               (1 << CAUSE_USER_ECALL) |                                \
-               (1 << CAUSE_BREAKPOINT);                                 \
-        csrw medeleg, t0;                                               \
-1:      csrwi mstatus, 0;                                               \
-        init;                                                           \
-        EXTRA_INIT;                                                     \
-        EXTRA_INIT_TIMER;                                               \
-        la t0, 1f;                                                      \
-        csrw mepc, t0;                                                  \
-        csrr a0, mhartid;                                               \
-        mret;                                                           \
-1:
+#define RVTEST_CODE_BEGIN                                                \
+        .section .text._start;                                           \
+        .globl _start;                                                   \
+        .align 2;                                                        \
+_start:                                                                  \
+        .option push;                                                    \
+        .option norelax;                                                 \
+        la gp, __global_pointer$;                                        \
+        .option pop;                                                     \
+        la sp, __stack_top;                                              \
+        INIT_XREG;                                                       \
+        li TESTNUM, 0                                                   
 
 //-----------------------------------------------------------------------
 // End Macro
@@ -260,20 +190,17 @@ reset_vector:                                                           \
 //-----------------------------------------------------------------------
 
 #define RVTEST_PASS                                                     \
-        fence;                                                          \
         li TESTNUM, 1;                                                  \
-        li a7, 93;                                                      \
         li a0, 0;                                                       \
         ecall
 
 #define TESTNUM gp
+
 #define RVTEST_FAIL                                                     \
-        fence;                                                          \
 1:      beqz TESTNUM, 1b;                                               \
         sll TESTNUM, TESTNUM, 1;                                        \
         or TESTNUM, TESTNUM, 1;                                         \
-        li a7, 93;                                                      \
-        addi a0, TESTNUM, 0;                                            \
+        li a0, 0;                                                       \
         ecall
 
 //-----------------------------------------------------------------------
